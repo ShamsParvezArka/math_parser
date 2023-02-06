1a1223ef54652ab21fde274583e37bebc29af532
Shams Parvez Arka <parvez6826@gmail.com>
2023-02-06 22:24:37 +0600

    implemented push down stack

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	printf("hello world\n");
+typedef struct Node {
+	double data;
+	struct Node *link;
+} node;
+node *head, *temp, *new_node;
+
+void push(double N);
+double pop(void);
+void print_linked_list(node *ref_node);
+
+int main()
+{
+	push(4);
+	push(5);
+	push(10);
+	while (head != NULL) {
+		printf("%.2lf ", head->data);
+		head = head->link;
+	}
 	return 0;
+}
+
+void push(double N) {
+	new_node = (node*) malloc(sizeof(node));
+	new_node->data = N;
+	new_node->link = head;
+	head = new_node;
+}
+double pop(void) {
+	temp = head;
+	head = head->link;
+	double popped_number = temp->data;
+	free(temp);
+	return popped_number;
 }
