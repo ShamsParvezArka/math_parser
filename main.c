d77dd79902c5b5f95fd04cc8a9b3ae2d440cb6ef
Shams Parvez Arka <parvez6826@gmail.com>
2023-02-07 01:05:21 +0600

    rebuild from scratch with proper blueprint

--- main.c
+++ main.c
@@ -2,37 +2,20 @@
 #include <stdlib.h>
 
 typedef struct Node {
-	double data;
+	char data;
 	struct Node *link;
 } node;
-node *head, *temp, *new_node;
-
-void push(double N);
-double pop(void);
-void print_linked_list(node *ref_node);
+node *new_raw_node;
 
 int main()
 {
-	push(4);
-	push(5);
-	push(10);
-	while (head != NULL) {
-		printf("%.2lf ", head->data);
-		head = head->link;
+	char ch = getchar();
+	while (ch != '\n') {
 	}
 	return 0;
 }
 
-void push(double N) {
-	new_node = (node*) malloc(sizeof(node));
-	new_node->data = N;
-	new_node->link = head;
-	head = new_node;
-}
-double pop(void) {
-	temp = head;
-	head = head->link;
-	double popped_number = temp->data;
-	free(temp);
-	return popped_number;
-}
+// Blueprint:
+// push(input: char) -> raw_list  <--
+// queue(raw_list) -> infix_list
+// infix_list -> postfix_list
