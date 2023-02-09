5c94b818f72b5db46a9f0d8d2c2a5c6a45a7f26a
Shams Parvez Arka <parvez6826@gmail.com>
2023-02-09 23:42:17 +0600

    push and queue function added

--- main.c
+++ main.c
@@ -3,26 +3,55 @@
 #include <string.h>
 #include <stdint.h>
 
-#define maxB 8
+typedef struct Node {
+	uint8_t data;
+	struct Node *link;
+} node;
+node *head, *new_node, *temp;
 
-uint8_t *arr;
-size_t bit_size;
-void take_input(void);
+void queue(uint8_t s_bit);
+void push(uint8_t s_bit);
 
 int main()
 {
-	take_input();
+	uint8_t ch = getchar();
+	while (ch != '\n') {
+		queue(ch);
+		ch = getchar();
+	}
+	push('x');
+
+	node *travel = malloc(sizeof(node));
+	travel = head;
+	while (travel != NULL) {
+		printf("%c", travel->data);
+		travel = travel->link;
+	}
+
 	return 0;
 }
 
-void take_input() {
-	arr = malloc(maxB * sizeof(uint8_t));
-	bit_size = 0;
-	uint8_t input = getchar();
-	while (input != '\n') {
-		arr[bit_size] = input;
-		bit_size++;
-		input = getchar();
+void queue(uint8_t s_bit) 
+{
+	new_node = (node*) malloc(sizeof(node));
+	new_node->data = s_bit;
+	new_node->link = NULL;
+
+	if (head == NULL) head = new_node;
+	else {
+		temp = (node*) malloc(sizeof(node));
+		temp = head;
+		while (temp->link != NULL) {
+			temp = temp->link;
+		}
+		temp->link = new_node;
 	}
-	arr = realloc(arr, bit_size * sizeof(uint8_t));
+}
+
+void push(uint8_t s_bit) 
+{
+	new_node = (node*) malloc(sizeof(node));
+	new_node->data = s_bit;
+	new_node->link = head;
+	head = new_node;
 }
